ef346c19a075249a9afd8e5bf374ebe3ce27a5b3
idanbuskila1 <100279949+idanbuskila1@users.noreply.github.com>
2023-05-27 22:13:56 +0300

    Update message_slot.h

--- message_slot.h
+++ message_slot.h
@@ -4,7 +4,6 @@
 #include <linux/ioctl.h>
 #define MAJOR_NUM 235
 #define BUF_LEN 128
-#define DEVICE_RANGE_NAME "char_dev"
 #define DEVICE_FILE_NAME "message_slot"
 
 #define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned long)
